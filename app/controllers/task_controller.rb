82cf5debec44d83eb9e1d854f81d3cfd40f08730
zhacatomn <zhacatomn@gmail.com>
2024-05-14 09:52:49 +0800

    define task controller

--- app/controllers/task_controller.rb
+++ app/controllers/task_controller.rb
@@ -5,4 +5,32 @@ class TaskController < ApplicationController
     def new:
         @task = Task.new
     end
+    def create:
+        @task = Task.new(task_params)
+        if @task.save
+            redirect_to @task
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
+    def edit:
+        @task = Task.find(params[:id])
+    end
+    def update:
+        @task = Task.find(params[:id])
+        if @task.update(task_params)
+            redirect_to @task
+        else 
+            render :edit, status: :unprocessable_entity
+        end
+    end
+    def destory:
+        @task = Task.find(params[:id])
+        @task.destory
+        redirect_to :index, status: :see_other
+    end
+    private
+    def task_params:
+        params.require(:task).permit(:title, :desc, :is_done)
+    end
 end
